713c2923d6ca53758a0ece21c2406b82472cf58f
Mark Richards <mark@wmrichards.com>
2020-03-19 09:34:56 -0400

    Carpool vs Ambulance Change
    Carpool means that the the priority thread only listens to priority messages; Ambulance means that the priority thread can handle other requests qhile no priority messages are there

--- ambulance/Receiver.java
+++ ambulance/Receiver.java
@@ -9,13 +9,15 @@ public class Receiver {
 		boolean PRIORITY = true;
 		
 		java.util.Scanner input = new java.util.Scanner(System.in);
-	    System.out.print("Priority Queue?(y/n): ");
+	    System.out.print("Priority Queue? (y/n): ");
 	    boolean priority = input.next().equalsIgnoreCase("y");
+	    System.out.print("Carpool Lane? (y/n): ");
+	    boolean carpool = input.next().equalsIgnoreCase("y");
 		input.close();
 		
 		if (priority) {
-			new Thread(()->new ReceiverThread().processMessages(PRIORITY)).start(); 
+			new Thread(()->new ReceiverThread().processMessages(PRIORITY, carpool)).start(); 
 		}
-		new Thread(()->new ReceiverThread().processMessages(STANDARD)).start(); 
+		new Thread(()->new ReceiverThread().processMessages(STANDARD, carpool)).start(); 
 	}
 }
--- ambulance/ReceiverThread.java
+++ ambulance/ReceiverThread.java
@@ -9,27 +9,47 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class ReceiverThread {
 
-	public void processMessages(boolean priority) {
+	public void processMessages(boolean priority, boolean carpool) {
 
 		try {
-			ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
+			ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616?jms.prefetchPolicy.all=0");
 			Connection connection = cf.createConnection();
 			connection.start();			
 			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			MessageConsumer receiver = null;
+			MessageConsumer priorityReceiver = null;
+			MessageConsumer standardReceiver = null;
 			if (priority) {
-				receiver = session.createConsumer(session.createQueue("PRIORITY.Q"));
+				priorityReceiver = session.createConsumer(session.createQueue("PRIORITY.Q"));
+				standardReceiver = session.createConsumer(session.createQueue("STANDARD.Q"));
 			} else {
-				receiver = session.createConsumer(session.createQueue("STANDARD.Q"));				
+				standardReceiver = session.createConsumer(session.createQueue("STANDARD.Q"));				
 			}
 
 			long time = 0;
+			String thread = "";
 			while (true) {
-				TextMessage msg = (TextMessage)receiver.receive();
-				if (msg.getText().equals("END")) break;
-				System.out.println("Processing message: " + msg.getText() + "  (T=" + time + ")");
-				Thread.sleep(2000);
-				time += 2000;
+				//if not carpool, always check priority queue first, then process a standard message
+				TextMessage msg = null;
+				if (priority) {
+					thread = "[PRIORITY THREAD]";
+					if (carpool) {
+						msg = (TextMessage)priorityReceiver.receive();
+					} else {
+						msg = (TextMessage)priorityReceiver.receiveNoWait();
+						if (msg == null) {
+							msg = (TextMessage)standardReceiver.receiveNoWait();
+						}
+					}
+				} else {
+					thread = "[NORMAL THREAD]";
+					msg = (TextMessage)standardReceiver.receive();
+				}
+				if (msg != null) {
+					if (msg.getText().equals("END")) break;
+					System.out.println("Processing message: " + msg.getText() + "  (T=" + time + ") - " + thread);
+					Thread.sleep(2000);
+					time += 2000;
+				}
 			}
 			
 			connection.close();			
